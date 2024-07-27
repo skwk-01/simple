26bccbb821794c51efc56824697676bc9b9351d7
skwk-01 <sksht.13@gmail.com>
2024-07-27 13:31:31 +0900

    first

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+import os 
+import uvicorn
+from fastapi import FastAPI, Depends
+from fastapi.responses import FileResponse
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+class Memo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100))
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    memo = Memo.query.all()
+    return render_template("index.html", memo=memo)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
